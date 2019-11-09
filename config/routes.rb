6013c3071456b7aec64e564ea037e9b8576c1738
Alice Gregson <gregsonalice1@gmail.com>
2019-11-09 11:35:10 +0000

    rails girls day

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get '/info', controller: 'pages', action: "info"
+  resources :ideas
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect("/ideas")
+  # automatically redirects to /ideas page in the brower (not the generic opening page/placeholder)
+
+end
