c42d23ac4f3da58ebbbd3541b1380ee9531bca47
Alice Gregson <gregsonalice1@gmail.com>
2019-11-09 15:07:21 +0000

    3rd commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_link(current_path, url_path)
+    "active" if current_path == url_path
+  end
 end
+
+# helpers are always available in other files
+# usually only avaiable in views and controllers (double check)
+# helpers used for little functions, that don't really fit anyone else
